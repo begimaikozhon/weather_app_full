0c3f7291dc6614374a17461ccc6a264669592640
begimaikozhon <kozhonova.begimai.kanybekovna@gmail.com>
2023-08-30 18:32:49 +0600

    add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,12 +10,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final api = 'https://jsonplaceholder.typicode.com/posts';
+  dynamic data;
 
   Future<dynamic> fetchData() async {
     final dio = Dio();
 
     final response = await dio.get(api);
-    print(response.data);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      data = response.data;
+    }
+    setState(() {});
   }
 
   @override
@@ -30,6 +34,13 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home page'),
       ),
+      body: data == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: Text('$data'),
+            ),
     );
   }
 }
