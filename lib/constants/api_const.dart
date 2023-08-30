db833570ad2b76ceb841824b1032f0677be4fc54
begimaikozhon <kozhonova.begimai.kanybekovna@gmail.com>
2023-08-30 19:04:23 +0600

    add api

---
+++ lib/constants/api_const.dart
@@ -0,0 +1,7 @@
+class ApiConst {
+  static const String apiKey = 'ced098463ae46cb45db3c6f0d4d4a086';
+  static const String cityNameOsh = 'osh';
+  static const String cityNameBishkek = 'bishkek';
+  static const String api =
+      'https://api.openweathermap.org/data/2.5/weather?q=$cityNameOsh&appid=$apiKey';
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,38 +9,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final api = 'https://jsonplaceholder.typicode.com/posts';
-  dynamic data;
-
-  Future<dynamic> fetchData() async {
-    final dio = Dio();
-
-    final response = await dio.get(api);
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      data = response.data;
-    }
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    fetchData();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home page'),
       ),
-      body: data == null
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Center(
-              child: Text('$data'),
-            ),
     );
   }
 }
